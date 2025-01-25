bf96bec99965fe416bf30883b8649ebfb376d70a
Lstark10 <lucas_mundo_louco@hotmail.com>
2025-01-25 20:59:35 -0300

    Manipulating elements and implementing a function for conversion

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-const amount = document.getElementById('amount')
+//cotação de moedas do dia
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
 
-amount.addEventListener('input', () => {
-  console.log(amount.value)
+// obtendo os elementos do formulario
+const form = document.querySelector('form')
+const amount = document.getElementById('amount')
+const currency = document.getElementById('currency')
+
+// manipulando o input amount para receber somente numeros
+amount.addEventListener("input", () => { 
+  const hasCharactersRegex = /\D+/g
+  amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+// Capturando o evento de submit do formulario
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break
+  }
+}
+
+// função para converter a moeda
+function convertCurrency(amount, price, symbol) {
+  console.log(amount, price, symbol)
+}
