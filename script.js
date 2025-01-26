de2854971f8711650f13be7f236f481a7ed1ca40
Lstark10 <lucas_mundo_louco@hotmail.com>
2025-01-26 12:17:32 -0300

    Displaying and converting the currency to Brazilian real

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const EUR = 5.32
 const GBP = 6.08
 
 // obtendo os elementos do formulario
-const form = document.querySelector('form')
-const amount = document.getElementById('amount')
-const currency = document.getElementById('currency')
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
 
 // manipulando o input amount para receber somente numeros
 amount.addEventListener("input", () => { 
@@ -33,5 +35,26 @@ form.onsubmit = (event) => {
 
 // função para converter a moeda
 function convertCurrency(amount, price, symbol) {
-  console.log(amount, price, symbol)
+  try {
+    // exibindo a cotação da moeda
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
+    
+    // aplica a classe que exibe o footer para mostrar o resultado
+    footer.classList.add("show-result")
+
+  } catch (error) {
+    // remove a classe do footer, removendo ele da tela
+    footer.classList.remove("show-result")
+    console.log(error)
+    alert("Não foi possível converter.")
+  }
+}
+
+// formatação da moeda em real brasileiro
+function formatCurrencyBRL(value) {
+  //converte para número para utilizar o toLocaleString para formatar no padrão BRL (R$ 00,00)
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+  })
 }
